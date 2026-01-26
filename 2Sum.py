a2d9b3af317d1148859602f861568fbc29d5cca1
Jerin8246 <154041891+Jerin8246@users.noreply.github.com>
2026-01-26 08:46:59 -0700

    solve 2Sum using python

---
+++ 2Sum.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = defaultdict(int)
+
+        for i in range(len(nums)):
+            map[nums[i]] = i
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in map and map[complement] != i:
+                return [i, map[complement]]
