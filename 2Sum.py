a822be1670f51671113917bc25cd593a83c2c815
Jerin8246 <154041891+Jerin8246@users.noreply.github.com>
2026-01-26 08:55:18 -0700

    update 2Sum

--- 2Sum.py
+++ 2Sum.py
@@ -10,3 +10,28 @@ class Solution:
             complement = target - nums[i]
             if complement in map and map[complement] != i:
                 return [i, map[complement]]
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = defaultdict(int)
+
+        for i in range(len(nums)):
+            map[nums[i]] = i
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in map and map[complement] != i:
+                return [i, map[complement]]
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        map = defaultdict(int)
+
+        for i, num in enumerate(nums):
+            if target - num in map:
+                return [i , map[target - num]]
+            map[num] = i
